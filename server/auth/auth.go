56a20e9f034d288880fd433e274443ede0f7a04e
Mjishu <128941980+Mjishu@users.noreply.github.com>
2025-01-02 16:18:45 -0500

    added jwt creation, might be broken

--- server/auth/auth.go
+++ server/auth/auth.go
@@ -1,6 +1,11 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"golang.org/x/crypto/bcrypt"
+	"github.com/golang-jwt/jwt" // go get this (should be this one https://github.com/golang-jwt/jwt
+)
+
+// go get uuid?
 
 func HashPassword(password string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 0)
@@ -13,3 +18,19 @@ func CheckPasswordHash(password, hash string) error {
 	bcrypt.CompareHashAndPassword([]byte(password), []byte(hash))
 	return nil
 }
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, 
+			 jwt.RegisteredClaims{
+				 "iss": "pokeFind-api",
+				 "iat": time.currentTime //utc
+				 "exp": time.currentTime + expiresIn
+				 "sub" : string(userID) //stringify user id
+			 })
+	tokenString, err := token.SignedString(tokenSecret)
+}
+
+// further implement the validate function
+// func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID,error) {
+// 	token, err := jwt.ParseWithClaims(tokenString, )
+// }
