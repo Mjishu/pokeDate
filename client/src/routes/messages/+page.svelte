22c53ab132b4585131d52ec2d0831e20dc55809e
mjishu <128941980+Mjishu@users.noreply.github.com>
2025-01-15 18:06:18 -0500

    added messages page

---
+++ client/src/routes/messages/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+      //todo make a layout for messages and children that shows the messages on the left side and then we can update the inner/whitespace part with the messages
+      // when they get selected
+
+</script>
+
+<h1>Messages</h1>
+
+<style>
+
+</style>
